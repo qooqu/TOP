0230d83d1a48cd4932a7b12e2af58e0c5b0249b4
qooqu <qooqu@users.noreply.github.com>
2021-02-08 18:13:31 +0000

    node library heroku tweaks

--- node/library/app.js
+++ node/library/app.js
@@ -12,11 +12,14 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
 
+var compression = require("compression");
+var helmet = require("helmet");
+
 var app = express();
 
 //Set up mongoose connection
 var mongoose = require("mongoose");
-var mongoDB = process.env.mongoSecret;
+var mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
@@ -25,10 +28,12 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+app.use(helmet());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); //Compress all routes
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
